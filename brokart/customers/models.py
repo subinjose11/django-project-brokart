1f93de605dcdf9e75ecc49e26cbae0fe7c1080c3
subinjose11 <subinjose911@gmail.com>
2025-09-11 08:12:31 +0530

    customer,product model created

--- brokart/customers/models.py
+++ brokart/customers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    LIVE = 1
+    DELETED = 0
+    DELETE_CHOICES = [
+        (LIVE, 'Live'),
+        (DELETED, 'Deleted'),
+    ]
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    address = models.TextField()
+    user=models.OneToOneField(User, on_delete=models.CASCADE,related_name='customer_profile')
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
--- brokart/products/models.py
+++ brokart/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Model 
+class Product(models.Model):
+    LIVE = 'live'
+    DELETED = 'deleted'
+    DELETE_CHOICES = [
+        (LIVE, 'Live'),
+        (DELETED, 'Deleted'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='media/', null=True, blank=True)
+    priority = models.IntegerField(default=0)
+    delete_status = models.CharField(max_length=10, choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
