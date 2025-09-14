6fe1487d1d79cf5b012b6b1d28894367582ab5e6
subinjose11 <subinjose911@gmail.com>
2025-09-14 20:01:19 +0530

    feat: Data model design

--- brokart/brokart/settings.py
+++ brokart/brokart/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'themes',
+    'products',
+    'customers',
+    'orders',
 ]
 
 MIDDLEWARE = [
--- brokart/orders/models.py
+++ brokart/orders/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from customers.models import Customer
+from products.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    LIVE = 1
+    DELETED = 0
+    DELETE_CHOICES = [
+        (LIVE, 'Live'),
+        (DELETED, 'Deleted'),
+    ]
+    CART_STAGE=0
+    ORDER_CONFIRMED=1
+    ORDER_PROCESSED=2
+    ORDER_DELIVERED=3
+    ORFER_REJECTED =4
+    STATUS_CHOICES = [
+        (CART_STAGE, 'Cart Stage'),
+        (ORDER_CONFIRMED, 'Order Confirmed'),
+        (ORDER_PROCESSED, 'Order Processed'),
+        (ORDER_DELIVERED, 'Order Delivered'),
+        (ORFER_REJECTED, 'Order Rejected'),
+    ]
+    order_status = models.IntegerField(choices=STATUS_CHOICES, default=CART_STAGE)
+    user = models.ForeignKey(Customer, on_delete=models.SET_NULL,related_name='orders',null=True)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.name
+
+class OrderedItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,related_name='added_cart',null=True)
+    quantity = models.IntegerField(default=1)
+    owner = models.ForeignKey(Customer, on_delete=models.SET_NULL,related_name='ordered_items',null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='cart_items')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.title
